0b050babcd85fc845065dd0b5b8ff5967fcfb90d
Siyuan Li <44519745+Lupin1998@users.noreply.github.com>
2020-09-30 13:42:36 +0800

    Add files via upload

--- invMLEnc_toy/run_test.sh
+++ invMLEnc_toy/run_test.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python main.py -SD 3 -Name Orth -T 1
-CUDA_VISIBLE_DEVICES=1 python main.py -SD 3 -Name Orth2 -T 2
-CUDA_VISIBLE_DEVICES=2 python main.py -SD 3 -Name Orth3 -T 3
-CUDA_VISIBLE_DEVICES=3 python main.py -SD 3 -Name Orth4 -T 4
+CUDA_VISIBLE_DEVICES=0 python main.py -SD 3 -Name Test -T 1
+CUDA_VISIBLE_DEVICES=1 python main.py -SD 3 -Name Test2 -T 2
+CUDA_VISIBLE_DEVICES=2 python main.py -SD 3 -Name Test3 -T 3
+CUDA_VISIBLE_DEVICES=3 python main.py -SD 3 -Name Test4 -T 4
