1a1225ef1f1b377d0039fe6223ff32d025600071
Siyuan Li <44519745+Lupin1998@users.noreply.github.com>
2020-09-30 13:43:48 +0800

    Add files via upload

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,8 @@
 CUDA_VISIBLE_DEVICES=0 python main.py -SD 3 -Name Test -T 1
-CUDA_VISIBLE_DEVICES=1 python main.py -SD 3 -Name Test2 -T 2
-CUDA_VISIBLE_DEVICES=2 python main.py -SD 3 -Name Test3 -T 3
-CUDA_VISIBLE_DEVICES=3 python main.py -SD 3 -Name Test4 -T 4
+# CUDA_VISIBLE_DEVICES=1 python main.py -SD 3 -Name Test2 -T 2
+# CUDA_VISIBLE_DEVICES=2 python main.py -SD 3 -Name Test3 -T 3
+# CUDA_VISIBLE_DEVICES=3 python main.py -SD 3 -Name Test4 -T 4
+# CUDA_VISIBLE_DEVICES=4 python main.py -SD 3 -Name Test5 -T 5
+# CUDA_VISIBLE_DEVICES=5 python main.py -SD 3 -Name Test6 -T 6
+# CUDA_VISIBLE_DEVICES=6 python main.py -SD 3 -Name Test7 -T 7
+# CUDA_VISIBLE_DEVICES=7 python main.py -SD 3 -Name Test8 -T 8
